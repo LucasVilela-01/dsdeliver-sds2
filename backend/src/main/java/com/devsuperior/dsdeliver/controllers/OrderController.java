01a619e1d933e399f00e7529246ba0d64828c2e7
Lucas Vilela <lrvilelab@hotmail.com>
2021-01-05 00:30:38 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -1,11 +1,16 @@
 package com.devsuperior.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
 import com.devsuperior.dsdeliver.services.OrderService;
@@ -17,8 +22,20 @@ public class OrderController {
 	@Autowired
 	private OrderService service;
 	
+	@GetMapping
 	public ResponseEntity<List<OrderDTO>> findAll() { 
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list); 
+	}
+	
+	@PostMapping
+	// @RequestBody - converte o JSON no objeto Java do tipo OrderDTO
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto);
+		// É uma chamada que faz para criar a uri que corresponde ao recurso criado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		// código 201 é quando um recurso é criado
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,29 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrdersWithProducts(); // repository retorna entidade
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		// Salvar no banco de dados
+		// Instant.now() -  instanciando um objeto de instant com o instant atual
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		// Antes de salvar no banco, temos associar esse pedido com os produtos que vieram no dto também
+		for (ProductDTO p : dto.getProducts()) {
+			/* .getOne - instancia um produto, mas não vai no banco de dados, simplesmente cria uma entidade gerenciada pela JPA, para que quando o pedido for
+			salvo, ele também salve as associações de quais produtos estão nesse pedido */
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		// Salvar o pedido no banco
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }
