fbb417cac9deee2a616d9ff5776e2eca25db9fbf
Lucas Vilela <lrvilelab@hotmail.com>
2021-01-06 19:22:34 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+// import Hello from './Hello'; // Importando o componente "Hello", from dizer entre '' qual o diretório, como está no mesmo diretório, usa-se ./
+
+
+function App() {
+ // return <Hello message="Lucas"/>; // Mostrando o componente "Hello"
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+}
+
+export default App;
